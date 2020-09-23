cdd3c86266af3d890bd97a76802d58e61ece996e
ALEX ZHOU <alex.zhou@ximalaya.com>
2020-09-23 19:54:29 +0800

    add
    Signed-off-by: ALEX ZHOU <alex.zhou@ximalaya.com>

---
+++ Example/Pods/Target Support Files/ANKit/ANKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ANKit : NSObject
+@end
+@implementation PodsDummy_ANKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-ANKit_Tests/Pods-ANKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ANKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ANKit_Tests
+@end
